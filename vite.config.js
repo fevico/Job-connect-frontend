b0d0ca96ace6825684615d38a0d4bfd01505eabb
Ajayi favour <97252800+fevico@users.noreply.github.com>
2024-08-24 16:10:15 +0100

    Update vite.config.js
    build update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // Import path module using ES module syntax
+import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'public'), // Adjust the path as necessary
+  root: path.resolve(__dirname, 'src'), // Use 'src' as the root directory
+  build: {
+    outDir: path.resolve(__dirname, 'dist'), // Output directory for the build
+  },
   plugins: [react()],
   resolve: {
     alias: {
@@ -11,4 +14,3 @@ export default defineConfig({
     },
   },
 });
-
