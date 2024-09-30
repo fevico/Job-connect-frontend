ffa660af71a3cf27415220ee0d835b5fc14a866b
John Chinonso Edeh <netojaycee@gmail.com>
2024-09-30 10:30:45 +0100

    vcb

--- src/redux/appData.js
+++ src/redux/appData.js
@@ -5,8 +5,8 @@ import { jwtDecode } from "jwt-decode";
 import Cookies from "js-cookie";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/",
-  // baseUrl: "https://jobkonnecta.com/api/",
+  // baseUrl: "http://localhost:5000/",
+  baseUrl: "https://jobkonnecta.com/api/",
   prepareHeaders: (headers) => {
     headers.set("Content-Type", "application/json");
     const token = Cookies.get("authToken");
